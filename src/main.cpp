005eee0e58792f840ae7f9fdef657a53d7d3a9ef
PhilippTheServer <philipp.lehmann@gruppe.ai>
2026-02-14 22:56:20 +0100

    feat(rules): add comment for rule descriptions

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 
+
+// game logic
+// Rules:
+// 1. A Cell that has less then 2 neighboors dies.
+// 2. A Cell that has 2 - 3 alive neighboors lives.
+// 3. A Cell that has more then 3 alive neighboors dies.
+// 4. A Cell that hsa 3 alive neighboors gets alive too.
+
+
+
+
+
+
+
+
+
 int main() {
     std::cout << "\nGAME OF Life\n";
     return 0;
